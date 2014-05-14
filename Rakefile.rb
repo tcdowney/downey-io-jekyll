3db55482a99480e87512d15554301439d0daecd4
Tim Downey <timcdowney@gmail.com>
2014-05-13 20:13:44 -0500

    Added Rake tasks for automatic Jekyll build and s3_website push

---
+++ Rakefile.rb
@@ -0,0 +1,14 @@
+# Remembering to build before I pushed to s3 was getting tedious
+
+namespace :site do
+  desc 'Builds and deploys all Jekyll sites with an s3_website.yml file'
+  task :deploy do
+    subfolders = Dir.glob('*').select {|f| File.directory? f}
+
+    subfolders.each do |subfolder|
+      if File.exists?(subfolder + '/s3_website.yml')
+        sh "cd #{ subfolder }; jekyll build; s3_website push --headless"
+      end
+    end
+  end
+end
