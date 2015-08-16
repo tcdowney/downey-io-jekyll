5e88cf8b152ff8eb6d8a9d6d273aa02c1d45fdf3
Tim Downey <timcdowney@gmail.com>
2015-08-16 00:32:27 -0500

    Rake tasks fail hard when sw_website.yml is missing

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,26 +3,24 @@
 namespace :deploy do
   desc 'Builds and deploys all Jekyll sites with an s3_website.yml file'
   task :all do
-    subfolders = Dir.glob('*').select { |f| File.directory? f }
-
-    subfolders.each do |subfolder|
-      if File.exists?(subfolder + '/s3_website.yml')
-        sh "cd #{ subfolder }; jekyll build; s3_website push"
-      end
+    ['blog', 'photo'].each do |subfolder|
+      fail "#{subfolder}/s3_website.yml does not exist" unless File.exists?("#{subfolder}/s3_website.yml")
+        
+      sh "cd #{ subfolder }; jekyll build; s3_website push"
     end
   end
 
   desc 'Builds and deploys my blog to downey.io on s3'
   task :blog do
-    if File.exists?('blog' + '/s3_website.yml')
-      sh 'cd blog; jekyll build; s3_website push'
-    end
+    fail 'blog/s3_website.yml does not exist' unless File.exists?('blog/s3_website.yml')
+      
+    sh 'cd blog; jekyll build; s3_website push'
   end
 
   desc 'Builds and deploys my photo site to photo.downey.io on s3'
   task :photo do
-    if File.exists?('photo' + '/s3_website.yml')
-      sh 'cd photo; jekyll build; s3_website push'
-    end
+    fail 'photo/s3_website.yml does not exist' unless File.exists?('photo/s3_website.yml')
+      
+    sh 'cd photo; jekyll build; s3_website push'
   end
 end
