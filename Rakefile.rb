9a3a950e97acca9caa83f95af3a442d842dca24a
Tim Downey <tim@downey.io>
2018-02-25 22:30:36 -0800

    Update rake tasks to set JEKYLL_ENV to production
    - this will ensure that jekyll-assets minifies and compresses
      assets as expected

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,22 +5,22 @@ namespace :deploy do
   task :all do
     ['blog', 'photo'].each do |subfolder|
       fail "#{subfolder}/s3_website.yml does not exist" unless File.exists?("#{subfolder}/s3_website.yml")
-        
-      sh "cd #{ subfolder }; jekyll build; s3_website push"
+
+      sh "cd #{ subfolder }; JEKYLL_ENV=production jekyll build; s3_website push"
     end
   end
 
   desc 'Builds and deploys my blog to downey.io on s3'
   task :blog do
     fail 'blog/s3_website.yml does not exist' unless File.exists?('blog/s3_website.yml')
-      
-    sh 'cd blog; jekyll build; s3_website push'
+
+    sh 'cd blog; JEKYLL_ENV=production jekyll build; s3_website push'
   end
 
   desc 'Builds and deploys my photo site to photo.downey.io on s3'
   task :photo do
     fail 'photo/s3_website.yml does not exist' unless File.exists?('photo/s3_website.yml')
-      
-    sh 'cd photo; jekyll build; s3_website push'
+
+    sh 'cd photo; JEKYLL_ENV=production jekyll build; s3_website push'
   end
 end
