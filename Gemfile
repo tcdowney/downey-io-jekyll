90e1ab3fb75fcd6630ca9a56ecb7005fdeebf376
Tim Downey <tim@downey.io>
2018-12-09 13:30:08 -0800

    Rubocop lint updates

--- Gemfile
+++ Gemfile
@@ -2,14 +2,14 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-assets'
-gem 'jekyll-sitemap'
-gem 'jekyll-redirect-from'
 gem 'jekyll-paginate'
+gem 'jekyll-redirect-from'
+gem 'jekyll-sitemap'
 
 gem 'bourbon', '~> 4.3.4'
 gem 'neat', '~> 1.0'
 
 gem 'rouge'
+gem 'rubocop'
 gem 's3_website'
 gem 'therubyracer'
-gem 'rubocop'
--- Rakefile.rb
+++ Rakefile.rb
@@ -1,23 +1,23 @@
 namespace :deploy do
   desc 'Builds and deploys all Jekyll sites with an s3_website.yml file'
   task :all do
-    ['blog', 'photo'].each do |subfolder|
-      fail "#{subfolder}/s3_website.yml does not exist" unless File.exists?("#{subfolder}/s3_website.yml")
+    %w[blog photo].each do |subfolder|
+      raise "#{subfolder}/s3_website.yml does not exist" unless File.exist?("#{subfolder}/s3_website.yml")
 
-      sh "cd #{ subfolder }; JEKYLL_ENV=production jekyll build; s3_website push"
+      sh "cd #{subfolder}; JEKYLL_ENV=production jekyll build; s3_website push"
     end
   end
 
   desc 'Builds and deploys my blog to downey.io on s3'
   task :blog do
-    fail 'blog/s3_website.yml does not exist' unless File.exists?('blog/s3_website.yml')
+    raise 'blog/s3_website.yml does not exist' unless File.exist?('blog/s3_website.yml')
 
     sh 'cd blog; JEKYLL_ENV=production jekyll build; s3_website push'
   end
 
   desc 'Builds and deploys my photo site to photo.downey.io on s3'
   task :photo do
-    fail 'photo/s3_website.yml does not exist' unless File.exists?('photo/s3_website.yml')
+    raise 'photo/s3_website.yml does not exist' unless File.exist?('photo/s3_website.yml')
 
     sh 'cd photo; JEKYLL_ENV=production jekyll build; s3_website push'
   end
@@ -26,8 +26,8 @@ end
 namespace :test do
   desc 'Builds and all Jekyll sites'
   task :build do
-    ['blog', 'photo'].each do |subfolder|
-      sh "cd #{ subfolder }; JEKYLL_ENV=development jekyll build"
+    %w[blog photo].each do |subfolder|
+      sh "cd #{subfolder}; JEKYLL_ENV=development jekyll build"
     end
   end
 end
