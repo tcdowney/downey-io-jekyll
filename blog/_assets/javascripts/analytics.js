6f2391225576e3e1d737705b6044d0992a37ace1
Tim Downey <tim@downey.io>
2020-05-31 13:44:58 -0700

    adjust GA configuration

--- blog/_assets/javascripts/analytics.js
+++ blog/_assets/javascripts/analytics.js
@@ -3,6 +3,8 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-51848813-1', 'downey.io');
+ga('create', 'UA-51848813-1', 'downey.io', {
+  'storage': 'none'
+});
 ga('set', 'anonymizeIp', true);
 ga('send', 'pageview');
